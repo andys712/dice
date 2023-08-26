45b5d0734c1e874bfe6799e124fdfe8c4f7f2f36
andys712 <105613249+andys712@users.noreply.github.com>
2023-08-26 23:31:05 +0100

    Added Function CAll

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
+function RollDice () {
     if (ok) {
         ok = false
         music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.InBackground)
@@ -10,19 +10,12 @@ input.onGesture(Gesture.Shake, function () {
         basic.showNumber(randint(1, 6))
         ok = true
     }
+}
+input.onGesture(Gesture.Shake, function () {
+    RollDice()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (ok) {
-        ok = false
-        music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.InBackground)
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.Square)
-            basic.showIcon(IconNames.Chessboard)
-        }
-        music.stopAllSounds()
-        basic.showNumber(randint(1, 6))
-        ok = true
-    }
+    RollDice()
 })
 let ok = false
 ok = true
