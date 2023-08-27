6a979440788806f548a5860764665c6e0afe7195
andys712 <105613249+andys712@users.noreply.github.com>
2023-08-27 01:29:25 +0100

    Looping

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-def on_gesture_shake():
+def on_button_pressed_a():
+    global LoopCount
+    if ok:
+        LoopCount += -1
+        basic.show_string("" + str((LoopCount)))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def RollDice():
     global ok
     if ok:
         ok = False
         music.play(music.builtin_playable_sound_effect(soundExpression.twinkle),
             music.PlaybackMode.IN_BACKGROUND)
-        for index in range(2):
+        for index in range(LoopCount):
             basic.show_icon(IconNames.SQUARE)
             basic.show_icon(IconNames.CHESSBOARD)
         music.stop_all_sounds()
         basic.show_number(randint(1, 6))
         ok = True
+
+def on_button_pressed_b():
+    global LoopCount
+    if ok:
+        LoopCount += 1
+        basic.show_string("" + str((LoopCount)))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    RollDice()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_logo_pressed():
-    global ok
-    if ok:
-        ok = False
-        music.play(music.builtin_playable_sound_effect(soundExpression.twinkle),
-            music.PlaybackMode.IN_BACKGROUND)
-        for index2 in range(2):
-            basic.show_icon(IconNames.SQUARE)
-            basic.show_icon(IconNames.CHESSBOARD)
-        music.stop_all_sounds()
-        basic.show_number(randint(1, 6))
-        ok = True
+    RollDice()
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
+LoopCount = 0
 ok = False
 ok = True
+LoopCount = 2
--- main.ts
+++ main.ts
@@ -1,21 +1,35 @@
+input.onButtonPressed(Button.A, function () {
+    if (ok) {
+        LoopCount += -1
+        basic.showString("" + LoopCount)
+    }
+})
 function RollDice () {
     if (ok) {
         ok = false
-        music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.InBackground)
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.Square)
-            basic.showIcon(IconNames.Chessboard)
-        }
-        music.stopAllSounds()
-        basic.showNumber(randint(1, 6))
-        ok = true
     }
+    music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.InBackground)
+    for (let index = 0; index < LoopCount; index++) {
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Chessboard)
+    }
+    music.stopAllSounds()
+    basic.showNumber(randint(1, 6))
+    ok = true
 }
+input.onButtonPressed(Button.B, function () {
+    if (ok) {
+        LoopCount += 1
+        basic.showString("" + LoopCount)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     RollDice()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     RollDice()
 })
+let LoopCount = 0
 let ok = false
 ok = true
+LoopCount = 2
